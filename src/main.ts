d717e93a0243d39887d3edb3b53a51ea3603c221
Nicolás Galdames <ngaldamesv@ripley.com>
2022-09-12 15:01:21 -0300

    :hammer: Modify Docker compose

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,26 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+	console.log(
+		process.env.SESSION_SECRET,
+		process.env.DB_HOST,
+		process.env.DB_PORT,
+		process.env.DB_USER,
+		process.env.DB_PASSWORD,
+		process.env.DB_NAME,
+		process.env.SPOTIFY_CLIENT_ID,
+		process.env.SPOTIFY_CLIENT_SECRET,
+		process.env.JWT_SECRET,
+		process.env.JWT_EXPIRES_IN,
+		process.env.PORT,
+		process.env.HOST_API,
+		process.env.HOST_SPOTIFY,
+		process.env.TOKEN_SPOTIFY,
+	);
 	const app = await NestFactory.create(AppModule);
 	const logger = new Logger('Bootstrap');
 	const configService = app.get(ConfigService);
+
 	app.setGlobalPrefix('api');
 	app.enableCors();
 	app.use(
@@ -24,6 +41,7 @@ async function bootstrap() {
 		}),
 	);
 	await app.listen(process.env.PORT || 3000);
+
 	logger.log(`Application listening on port ${process.env.PORT || 3000}`);
 }
 bootstrap();
--- src/modules/health/health.service.ts
+++ src/modules/health/health.service.ts
@@ -4,6 +4,8 @@ import { HealthInterface } from './interfaces';
 @Injectable()
 export class HealthService {
 	getHealthResponse(): HealthInterface {
+		const data = process.env;
+		console.log(data.DB_HOST, data.DB_PORT, data.DB_USER, data.DB_PASSWORD, data.DB_DATABASE);
 		return { status: 'ok' };
 	}
 }
